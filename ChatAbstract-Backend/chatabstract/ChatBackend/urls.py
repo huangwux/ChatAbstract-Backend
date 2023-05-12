1724737c4485a43250a296e221069672157326f0
huangwux <113427203+huangwux@users.noreply.github.com>
2023-05-12 09:24:21 +0800

    Update urls.py

--- ChatAbstract-Backend/chatabstract/ChatBackend/urls.py
+++ ChatAbstract-Backend/chatabstract/ChatBackend/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from .views import *
+
 
 urlpatterns = [
-    path("", views.init, name="init"),
-    path("", views.chat, name="chat"),
+    path("chat/init/", init, name='init'),
+    path("chat/chat/", chat, name='chat'),
+    path("chat/respond/string/", respond_with_string, name='respond_with_string'),
+    path("chat/respond/json/", respond_with_json, name='respond_with_json'),
+    path("store/op/", user_op, name='user_op')
 ]
