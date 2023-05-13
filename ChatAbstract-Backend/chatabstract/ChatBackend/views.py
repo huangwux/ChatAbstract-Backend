4170d5b10a392d3c062c6c0c04bda1f972266773
huangwux <113427203+huangwux@users.noreply.github.com>
2023-05-13 18:43:27 +0800

    Update views.py

--- ChatAbstract-Backend/chatabstract/ChatBackend/views.py
+++ ChatAbstract-Backend/chatabstract/ChatBackend/views.py
@@ -4,6 +4,30 @@ import json
 import re
 
 
+def generate_prompt(operation) -> str:
+    """generate prompts based on users' operations
+
+    Args:
+        operation (dict): a dict containing the user's operation in the following format
+        operation = 
+        {
+            "text": "I eat food. I likes to eat food. I love hamburgers.",
+            "start_index": 0,
+            "end_index": 11,
+            "comment": "Change to passive voice"
+        }
+
+    Returns:
+        str: the prompt
+    """
+    prompt = "The original text is <" + operation["text"]+">. "
+    sentence = operation["text"][operation["start_index"]:operation["end_index"]]
+    prompt += "For sentence <" + sentence + ">, whose start index is " + str(operation["start_index"]) + " and end index is " + str(operation["end_index"]) + ", " + "the user's comment is " + operation["comment"] + "."
+    prompt += "Notice only the sentence mentioned should be changed. Give me the changed version of the whole text, along with the changed part's start index and end index in the form of json, with three keys: text, start_index and end_index.  Your response should only contain json data. If the action is deletion, omit the start_index and end_index keys in your response."
+    return prompt
+
+
+
 def init(request): # stat conversation with gpt
     text = "I want you to act as my academic writing mentor and polish my essay according to my instructions."
     os.environ["http_proxy"] = "http://127.0.0.1:12935"
@@ -76,7 +100,17 @@ def respond_with_string(request): # get answer and return the answer with json f
 
 def respond_with_json(request): # get answer and return the answer with json form
     text = request.POST.get('text')
-    print(text)
+    start = request.POST.get('start_index')
+    end = request.POST.get('end_index')
+    comment = request.POST.get('comment')
+    operation = {
+        "text": text,
+        "start_index": start,
+        "end_index": end,
+        "comment": comment
+    }
+    prompt = generate_prompt(operation)
+    #rint(text)
     os.environ["http_proxy"] = "http://127.0.0.1:12935"
     os.environ["https_proxy"] = "http://127.0.0.1:12935"
     openai.api_key = "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"
@@ -85,36 +119,32 @@ def respond_with_json(request): # get answer and return the answer with json for
         temperature=0.8,
         max_tokens=10000,
         messages=[
-            {"role": "user", "content": f"{text}"}
+            {"role": "user", "content": prompt}
         ]
     )
 
     response = res.choices[0].message["content"]
     #print(response)
     #length_of_answer=len(response)
-    start_index=re.findall(r'"start_index": ([0-9]*?),', response)
-    end_index=re.findall(r'"end_index": ([0-9]*?),', response)
-    label=re.findall(r'"comment": (.*?),', response)
+    #start_index=re.findall(r'"start_index": ([0-9]*?),', response)
+    #end_index=re.findall(r'"end_index": ([0-9]*?),', response)
+    #label=re.findall(r'"comment": (.*?),', response)
 
-    answer=json.dumps({'start': start_index, 'end': end_index , 'label':label}, sort_keys=True, indent=4, 
-    separators=(',', ': '))
+    #answer=json.dumps({'start': start_index, 'end': end_index , 'label':label}, sort_keys=True, indent=4, 
+    #separators=(',', ': '))
 
     chat = Chat.objects.create(
         text=text,
         gpt=response
     )
     chat.save()
-    return(answer)
 
-def user_op(request): #store user's operation to database
-    op_type=request.POST.get('type')
-    op_pos_start=request.POST.get('pos_start')
-    op_pos_end=request.POST.get('pos_end')
-    op_content=request.POST.get('content')
-    op=Operation.objects.create(
-        type=op_type,
-        pos_start=op_pos_start,
-        pos_end= op_pos_end,
-        content=op_content
+    op= Operation.objects.create(
+        text=text,
+        pos_start=start,
+        pos_end=end,
+        comment=comment
     )
     op.save()
+    return(response)
+
