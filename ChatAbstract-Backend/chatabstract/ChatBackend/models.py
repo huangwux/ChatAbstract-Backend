e39c51c7ffd2ff669d385147a6f292808a839b05
huangwux <113427203+huangwux@users.noreply.github.com>
2023-05-15 00:00:49 +0800

    Update models.py

--- ChatAbstract-Backend/chatabstract/ChatBackend/models.py
+++ ChatAbstract-Backend/chatabstract/ChatBackend/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class Chat(models.Model):
     text = models.TextField(max_length=10000)
     gpt = models.TextField(max_length=20000)
-    datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    #datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
 class Operation(models.Model):
     text = models.IntegerField(blank=True)
     pos_start = models.IntegerField(blank=True)
     pos_end = models.IntegerField(blank=True)
     comment = models.TextField(max_length=20000)
-    datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    #datetime = models.DateTimeField(auto_now_add=True, blank=True, null=True)
